fb977237cd06bac83836984ae61554e83636e642
Anostosia117 <jigsa@yandex.ru>
2022-04-20 22:46:19 +0300

    задание 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,29 @@
 package com.company;
 
 public class Main {
+
     public static void main(String[] args) {
-
-
+        int year = 2006;
+        isYearLeap(year);
+    }
+    public static void isYearLeap(int year) {
+        if (year % 400 == 0) {
+            System.out.println(year + " високосный год");
+        } else {
+            System.out.println(year + " не високосный год");
+        }
 
     }
+    public static void task2() {
+        // Здесь пишем код второго задания
     }
+    public static void task3() {
+        // Здесь пишем код первого задания
+    }
+    public static void task4() {
+        // Здесь пишем код первого задания
+    }
+    public static void task5() {
+        // Здесь пишем код первого задания
+    }
+}
